38f0da7b7240a6659de4978853ca4c164208bae4
PratikS447 <shindepratik447@gmail.com>
2026-01-31 01:45:46 +0530

    print numbers

--- src/main/java/org/example/Basic/RecurrsionBasic.java
+++ src/main/java/org/example/Basic/RecurrsionBasic.java
@@ -1,5 +1,24 @@
 package org.example.Basic;
 
 public class RecurrsionBasic {
+    public static void printnNumber(int n, int i){
+        if (n < i){
+            return;
+        }
+        System.out.print(i+" ");
+        printnNumber(n, i+1);
+    }
 
+    public static void reverseNumber(int n){
+        if (n == 0){
+            return;
+        }
+        System.out.print(n+" ");
+        reverseNumber(n-1);
+    }
+
+    public static void main(String args[]){
+//        printnNumber(5, 1);
+        reverseNumber(5);
+    }
 }
