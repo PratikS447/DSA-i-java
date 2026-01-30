6d145af9192352251828213b42fa59c7c5fa7af5
PratikS447 <shindepratik447@gmail.com>
2026-01-31 02:01:00 +0530

    Sum of irstt n numbers.

--- src/main/java/org/example/Basic/RecurrsionBasic.java
+++ src/main/java/org/example/Basic/RecurrsionBasic.java
@@ -1,6 +1,8 @@
 package org.example.Basic;
 
 public class RecurrsionBasic {
+
+//    Print 1 to N using Recursion
     public static void printnNumber(int n, int i){
         if (n < i){
             return;
@@ -9,6 +11,7 @@ public class RecurrsionBasic {
         printnNumber(n, i+1);
     }
 
+//    Print N to 1 using Recursion
     public static void reverseNumber(int n){
         if (n == 0){
             return;
@@ -17,8 +20,18 @@ public class RecurrsionBasic {
         reverseNumber(n-1);
     }
 
+//    Sum of first n numbers
+    public static int sum(int n){
+        if (n == 0){
+            return 0;
+        }
+
+        return n + sum(n-1);
+    }
+
     public static void main(String args[]){
 //        printnNumber(5, 1);
-        reverseNumber(5);
+//        reverseNumber(5);
+        System.out.print(sum(5));
     }
 }
