04753c154d73784a3d67e21973650cd65b5991e7
PratikS447 <shindepratik447@gmail.com>
2026-01-31 02:45:48 +0530

    Check palidrom

--- src/main/java/org/example/Basic/RecurrsionBasic.java
+++ src/main/java/org/example/Basic/RecurrsionBasic.java
@@ -29,9 +29,52 @@ public class RecurrsionBasic {
         return n + sum(n-1);
     }
 
+    public static int sum1(int currSum, int n){
+        if (n == 0){
+            return currSum;
+        }
+
+        return sum1(currSum+n, n-1);
+    }
+
+//    factorial of n numbers
+    public static int fact(int n){
+        if (n < 1){
+            return 1;
+        }
+        return n * fact(n-1);
+    }
+
+//    REVERSE AN ARRAY
+    public static void reveArray(int [] arr, int idx){
+        if (idx == arr.length){
+            return;
+        }
+        System.out.print(arr[arr.length -1 - idx]+" ");
+        reveArray(arr, idx+1);
+    }
+
+//    CHECK IF STRING IS PALIDROM OR NOT
+    public static boolean isPalidrom(String str, int start, int end){
+        if (start >= end){
+            return true;
+        }
+        if (str.charAt(start) != str.charAt(end)){
+            return false;
+        }
+
+        return isPalidrom(str, start+1, end-1);
+    }
+
     public static void main(String args[]){
 //        printnNumber(5, 1);
 //        reverseNumber(5);
-        System.out.print(sum(5));
+//        System.out.print(sum(5));
+//        System.out.print(fact(5));
+//        System.out.print(sum1(0, 5));
+//        int arr[] = {1,2,3,4,5};
+//        reveArray(arr, 0);
+        String str = "madam";
+        System.out.print(isPalidrom(str, 0, str.length()-1));
     }
 }
