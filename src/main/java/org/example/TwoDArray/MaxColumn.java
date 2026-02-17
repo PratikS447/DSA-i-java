36eee3284a18f2cc97d752bf7e8da7cf111e5b92
PratikS447 <shindepratik447@gmail.com>
2026-02-18 00:04:57 +0530

    Max Column

--- src/main/java/org/example/TwoDArray/MaxColumn.java
+++ src/main/java/org/example/TwoDArray/MaxColumn.java
@@ -1,13 +1,20 @@
 package org.example.TwoDArray;
 
 public class MaxColumn {
-    public static void max(int arr[][]){
+    public static int max(int arr[][]){
+        int max = 0;
         for(int col = 0; col < arr.length; col++){
             int sum = 0;
             for (int row = 0; row < arr[0].length; row++){
-                sum = sum + arr[col][row];
-
+                sum = sum + arr[row][col];
             }
+            max = Math.max(max, sum);
         }
+        return max;
+    }
+
+    public static void main(String args[]){
+        int arr[][] = {{1,2,3},{4,5,6},{7,8,9}};
+        System.out.println("Maximum column in an array : "+ max(arr));
     }
 }
