58ddada845170664af3b09463941f51a5c25e735
PratikS447 <shindepratik447@gmail.com>
2026-02-25 19:05:56 +0530

    Special Matrix question..

--- src/main/java/org/example/TwoDArray/diagonalOrder.java
+++ src/main/java/org/example/TwoDArray/diagonalOrder.java
@@ -35,6 +35,23 @@ public class diagonalOrder {
         return ls;
     }
 
+    public static boolean special_Matrix(int mat[][]){
+        for(int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat[0].length; j++) {
+                if ((i + j) % 2 == 0) {
+                    if (mat[i][j] == 0) {
+                        return false;
+                    }
+                } else {
+                    if (mat[i][j] == 0) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
     public static void main(String args[]){
         int arr[][] = {
                 { 1, 2, 3, 4 },
