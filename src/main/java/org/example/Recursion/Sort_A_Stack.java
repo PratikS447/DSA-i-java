d9c09f05fd5bc128e956e3f5901ac451e8128ce1
PratikS447 <shindepratik447@gmail.com>
2026-03-06 16:08:53 +0530

    Sort a stack

---
+++ src/main/java/org/example/Recursion/Sort_A_Stack.java
@@ -0,0 +1,34 @@
+package org.example.Recursion;
+
+import java.util.Stack;
+
+public class Sort_A_Stack {
+    public static void myStack(Stack<Integer> st){
+        if(st.isEmpty()){
+            return;
+        }
+        int temp = st.pop();
+        myStack(st);
+        insertSort(st, temp);
+    }
+
+    public static void insertSort(Stack<Integer> st, int temp){
+        if(st.isEmpty() || st.peek() < temp){
+            st.push(temp);
+            return;
+        }
+        int ele = st.pop();
+        insertSort(st, temp);
+        st.push(ele);
+    }
+
+    public static void main(String args[]){
+        Stack<Integer> st = new Stack<>();
+        st.push(3);
+        st.push(1);
+        st.push(4);
+        st.push(2);
+        myStack(st);
+        System.out.println(st);
+    }
+}
