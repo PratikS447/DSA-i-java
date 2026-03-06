72cdc68ee217d04bf27a88717ceeb43437931424
PratikS447 <shindepratik447@gmail.com>
2026-03-06 06:59:10 +0530

    Atoi recursively

---
+++ src/main/java/org/example/Recursion/CountGoodNumber.java
@@ -0,0 +1,32 @@
+package org.example.Recursion;
+
+public class CountGoodNumber {
+    public static long helper(long base, long expo, long mod){
+        if(expo == 0){
+            return 1;
+        }
+
+        long half = helper(base, expo/2, mod);
+
+        if(expo % 2 == 0){
+            return (half*half)%mod;
+        }else {
+            return (base*half*half)%mod;
+        }
+    }
+
+    public static int myRecursiveMethod(int n){
+        if (n == 1) return 1;
+        long even = (n+1)/2;
+        long odd = n/2;
+        long mod = 100000000 + 7;
+
+        long result = (helper(5, even, mod) * helper(4, odd, mod)) % mod;
+
+        return (int)result;
+    }
+
+    public static void main(String args[]){
+        System.out.println(myRecursiveMethod(4));
+    }
+}
