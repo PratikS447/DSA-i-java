52dd6ff077dff9b8987d2a0581057edacdc43030
PratikS447 <shindepratik447@gmail.com>
2026-04-10 11:25:48 +0530

    abstraction, inheritance, polymorphism, encapsulation, complex number

---
+++ src/main/java/org/example/OOP/Abstraction/Employee.java
@@ -0,0 +1,12 @@
+package org.example.OOP.Abstraction;
+
+// Abstract class - cannot be instantiated directly
+abstract class Employee {
+    // Abstract method - no body, must be implemented by subclasses
+    abstract double calculateSalary();
+
+    // Concrete method
+    void displayRole() {
+        System.out.println("Employee role");
+    }
+}
---
+++ src/main/java/org/example/OOP/Abstraction/Payable.java
@@ -0,0 +1,6 @@
+package org.example.OOP.Abstraction;
+
+// Interface for pay calculation
+interface Payable {
+    void processPayment();
+}
