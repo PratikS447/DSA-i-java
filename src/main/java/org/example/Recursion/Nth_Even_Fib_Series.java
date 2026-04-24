27e2b2001d1444e306dae92e21f72ef7fc35ca63
PratikS447 <shindepratik447@gmail.com>
2026-04-24 14:49:01 +0530

    Nth even fib series

---
+++ src/main/java/org/example/Recursion/Nth_Even_Fib_Series.java
@@ -0,0 +1,14 @@
+package org.example.Recursion;
+
+public class Nth_Even_Fib_Series {
+    public static int even_Fib(int n){
+        if(n == 0) return 2;
+        if(n == 1) return 8;
+
+        return 4*even_Fib(n-1) + even_Fib(n-2);
+    }
+
+    public static void main(String args[]){
+        System.out.print(even_Fib(0));
+    }
+}
