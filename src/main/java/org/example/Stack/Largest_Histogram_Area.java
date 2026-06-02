965292593151d9d92445cb3f967228d7c6cad3b5
PratikS447 <shindepratik447@gmail.com>
2026-06-02 14:40:04 +0530

    largest histogram area

---
+++ src/main/java/org/example/Stack/Largest_Histogram_Area.java
@@ -0,0 +1,42 @@
+package org.example.Stack;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Stack;
+
+public class Largest_Histogram_Area {
+    public static int larg_histo_area(int arr[], int n){
+        int nse[] = new int[arr.length];
+        Stack<Integer> st_nse = new Stack<>();
+        Arrays.fill(nse, n);
+        for (int right = 0; right < arr.length; right++){
+            while (!st_nse.isEmpty() && arr[st_nse.peek()] > arr[right]){
+                nse[st_nse.pop()] = right;
+            }
+            st_nse.push(right);
+        }
+
+        int pse[] = new int[arr.length];
+        Stack<Integer> st_pse = new Stack<>();
+        Arrays.fill(pse, -1);
+        for (int left = n-1; left >= 0; left--){
+            while (!st_pse.isEmpty() && arr[st_pse.peek()] > arr[left]){
+                pse[st_pse.pop()] = left;
+            }
+            st_pse.push(left);
+        }
+
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++){
+            int width = nse[i] -pse[i] -1;
+            int area = width*arr[i];
+            max = Math.max(max, area);
+        }
+        return max;
+    }
+
+    public static void main(String args[]){
+        int arr[] = {2, 1, 5, 6, 2, 3};
+        System.out.println("Max Area: " + larg_histo_area(arr, arr.length)); // Expected: 10
+    }
+}
