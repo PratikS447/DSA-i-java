d3ffceb1a6d63364c604694bf801c16a2e694e8d
PratikS447 <shindepratik447@gmail.com>
2026-06-02 13:37:41 +0530

    next smaller element

---
+++ src/main/java/org/example/Stack/Next_Smaller_Element.java
@@ -0,0 +1,25 @@
+package org.example.Stack;
+
+import java.util.Arrays;
+import java.util.Stack;
+
+public class Next_Smaller_Element {
+    public static int[] next_smaller_ele(int arr[]){
+        Stack<Integer> stack = new Stack<>();
+        int nse[] = new int[arr.length];
+        Arrays.fill(nse, -1);
+        for (int right = 0; right < arr.length; right++){
+            while (!stack.isEmpty() && arr[stack.peek()] > arr[right]){
+                nse[stack.pop()] = arr[right];
+            }
+            stack.push(right);
+        }
+        return nse;
+    }
+
+    public static void main(String args[]){
+        int arr[] = {4, 5, 2, 10, 8};
+        System.out.println(Arrays.toString(next_smaller_ele(arr)));
+        // Correct Expected Output: [2, 2, -1, 8, -1]
+    }
+}
