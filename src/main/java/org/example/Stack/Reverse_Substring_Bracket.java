4e879d45e8ee9196a1d684f2154fd03dbd16709f
PratikS447 <shindepratik447@gmail.com>
2026-06-10 17:49:08 +0530

    Reverse substring between each pair of parentheses

---
+++ src/main/java/org/example/Stack/Reverse_Substring_Bracket.java
@@ -0,0 +1,38 @@
+package org.example.Stack;
+
+import java.util.Stack;
+
+public class Reverse_Substring_Bracket {
+    public static String reverse_substring(String nums, int n){
+        Stack<Character> stack = new Stack<>();
+        StringBuilder sb = new StringBuilder();
+        for (char num: nums.toCharArray()){
+            if(num == ')'){
+                while (!stack.isEmpty() && stack.peek() != '('){
+                    sb.append(stack.pop());
+                }
+
+                if(!stack.isEmpty()) stack.pop();
+
+                for (char c : sb.toString().toCharArray()){
+                    stack.push(c);
+                }
+
+                sb.setLength(0);
+            }else{
+                stack.push(num);
+            }
+        }
+
+        StringBuilder res = new StringBuilder();
+        while (!stack.isEmpty()){
+            res.append(stack.pop());
+        }
+
+        return res.reverse().toString();
+    }
+
+    public static void main(String args[]){
+        System.out.println(reverse_substring("(accio(job))", 12));
+    }
+}
