3a80f55c701980908a82333b8c207d0107149643
PratikS447 <shindepratik447@gmail.com>
2026-07-17 18:26:06 +0530

    Max width ramp

--- src/main/java/org/example/TwoPointer_SlidingWindow/Max_Width_Ramp.java
+++ src/main/java/org/example/TwoPointer_SlidingWindow/Max_Width_Ramp.java
@@ -1,7 +1,28 @@
 package org.example.TwoPointer_SlidingWindow;
 
 public class Max_Width_Ramp {
+    public static int max_ramp(int[] nums){
+        int suffix[] = suffix(nums);
+        int max = Integer.MIN_VALUE;
+        int i = 0, j = 0, n = nums.length;
+        while (i < n && j < n){
+            if(nums[i] <= suffix[j]){
+                max = Math.max(max, j -i);
+                j++;
+            }else{
+                i++;
+            }
+        }
+        return max;
+    }
 
-    public static void main(String args[]){
+    public static int[] suffix(int nums[]){
+        int max = Integer.MIN_VALUE;
+        int ans[] = new int[nums.length];
+        for (int i = nums.length -1; i >= 0; i--){
+            max = Math.max(max, nums[i]);
+            ans[i] = max;
+        }
+        return ans;
     }
 }
--- src/test/java/org/example/TwoPointer_SlidingWindow/Max_Width_Ramp_Test.java
+++ src/test/java/org/example/TwoPointer_SlidingWindow/Max_Width_Ramp_Test.java
@@ -1,4 +1,25 @@
 package org.example.TwoPointer_SlidingWindow;
 
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class Max_Width_Ramp_Test {
+    private int[] makeTest(int...arr){
+        return arr;
+    }
+
+    @Test
+    public void StandardTestCase(){
+        int arr[] = {6,0,8,2,1,5};
+        int [] input = makeTest(arr);
+        assertEquals(4, Max_Width_Ramp.max_ramp(input));
+    }
+
+    @Test
+    public void StandardTestCase2(){
+        int arr[] = {9,8,1,0,1,9,4,0,4,1};
+        int [] input = makeTest(arr);
+        assertEquals(7, Max_Width_Ramp.max_ramp(input));
+    }
 }
