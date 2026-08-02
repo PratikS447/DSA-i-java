765077d96eb19b038eb4f1fe672276c6e532958e
PratikS447 <shindepratik447@gmail.com>
2026-08-02 11:14:45 +0530

    find the ele

--- src/main/java/org/example/Tree/Binary_Search_Tree/BST_Search.java
+++ src/main/java/org/example/Tree/Binary_Search_Tree/BST_Search.java
@@ -1,4 +1,20 @@
 package org.example.Tree.Binary_Search_Tree;
 
 public class BST_Search {
+    public static boolean find(Node root, int data){
+        Node curr = root;
+        while(curr != null){
+            if(curr.data == data) return true;
+
+            else if (curr.data < data){
+                curr = curr.right;
+            }
+
+            else{
+                curr = curr.left;
+            }
+        }
+        return false;
+    }
+
 }
